d147c12ac67164f2cf288f3ad3b4e9d475c40cc2
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2020-05-31 18:09:37 -0700

    added budget and alert
    added budget and alert

--- MyExpenseTracker/MyExpenseTracker/CategoriesPage.xaml.cs
+++ MyExpenseTracker/MyExpenseTracker/CategoriesPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,12 +17,26 @@ namespace MyExpenseTracker
     public partial class CategoriesPage : ContentPage
     {
         public ObservableCollection<Categories> categories_oc { get; set; }
+        public string _budgetFile = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "budget.txt");
         public CategoriesPage()
         {
             InitializeComponent();
             categories_oc = new ObservableCollection<Categories>();
             CategoryManager.GetAllCategories(categories_oc);
             listView.ItemsSource = categories_oc;
-        }  
+
+            if (!File.Exists(_budgetFile))
+            {
+                // DisplayAlert("Alert", "You have been alerted", "OK");
+                goToBudgetPage();
+            }
+        }
+
+        public async void goToBudgetPage()
+        {
+            await DisplayAlert("Alert", "Please set the budget first. Redirecting to Budget page", "OK");
+            await Navigation.PushModalAsync(new BudgetPage());
+        }
     }
 }
